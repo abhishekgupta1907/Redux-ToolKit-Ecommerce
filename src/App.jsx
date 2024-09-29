6b4b9387d550d5b4dc2453439ccacec7b3931cc9
Abhishek Gupta <103306891+abhishekgupta1907@users.noreply.github.com>
2024-09-29 12:29:23 +0530

    adding css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Main from "./Components/Main/Main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import FilteredProducts from "./Components/FiltredProducts/FilteredProducts";
 import SingleProduct from "./Components/FiltredProducts/SingleProduct";
-import Login from "./Components/Login/Login";
-import { useSelector } from "react-redux";
+// import Login from "./Components/Login/Login";
+// import { useSelector } from "react-redux";
 
 function App() {
-    const user = useSelector((state) => state.user.user);
-    const { authUser } = user;
+    // const user = useSelector((state) => state.user.user);
+    // const { authUser } = user;
 
     return (
         <div className="App">
@@ -16,7 +16,8 @@ function App() {
                 <Routes>
                     <Route
                         path="/"
-                        element={authUser ? <Main /> : <Login />}
+                        // element={authUser ? <Main /> : <Login />}
+                        element={<Main />}
                     ></Route>
 
                     <Route
